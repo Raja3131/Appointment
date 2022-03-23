bd5775b025a81165ec4bac74e33faaea9613794d
Raja <kingraja673@gmail.com>
2022-03-23 18:53:23 +0530

    reschedule screen completed but need to complete UI

--- appointment/src/screens/MyAppoints.js
+++ appointment/src/screens/MyAppoints.js
@@ -1,132 +1,133 @@
-import React from 'react'
-import {View,Text,StyleSheet,Pressable } from 'react-native';
-import {useState,useEffect} from 'react';
+import React from 'react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
+import {useState, useEffect} from 'react';
 import Api from '../api/Api';
 
-
 const MyAppoints = ({navigation}) => {
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [appointments,setAppointments] = useState([]);
-    
+  const [appointments, setAppointments] = useState([]);
 
-    useEffect(()=>{
-        Api.get('/appoints')
-        .then(res=>{
-            setAppointments([...res.data.appoints]);
-            setIsLoading(false);
+  useEffect(() => {
+    Api.get('/appoints')
+      .then(res => {
+        setAppointments([...res.data.appoints]);
+        setIsLoading(false);
 
-            console.log(res.data);
-
-        })
-        .catch(err=>{
-            console.log(err);
-        })
-    },[
-        setAppointments,
-        setIsLoading,
-       
-    ])
-const OnDelete = (id) => {
-    Api.delete(`/appoints/${id}`)
-    .then(res=>{
-        console.log(res);
-        setAppointments(appointments.filter(appointment=>appointment._id !== id));
-    }).catch(err=>{
+        console.log(res.data);
+      })
+      .catch(err => {
         console.log(err);
-    })
-}
+      });
+  }, [setAppointments, setIsLoading]);
+  const OnDelete = id => {
+    Api.delete(`/appoints/${id}`)
+      .then(res => {
+        console.log(res);
+        setAppointments(
+          appointments.filter(appointment => appointment._id !== id),
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-const OnReschedule = (id) => {
-    navigation.navigate('Reschedule',{id:id});
+  const OnReschedule = (id, name, date, time) => {
+    navigation.navigate('Reschedule', {
+      id,
+      name,
+      date,
+      time,
+    });
+  };
 
-}
-    
-  
-   
-    return (
-        <View>
-            <Text style={styles.header}>My Appointments</Text>
-       
-        {appointments.map(appointment=>(
-            <View style={styles.appointment}>
-                <Text style={styles.nameText}>{appointment.name}</Text>
-                <Pressable
-                onPress={()=>OnReschedule(
-                    appointment._id
-                )}
-                style={styles.editButton}>
+  return (
+    <View>
+      <Text style={styles.header}>My Appointments</Text>
 
-                    <Text style={styles.editText}>Reschedule</Text>
+      {appointments.map(appointment => (
+        <View style={styles.appointment}>
+          <Text style={styles.nameText}>{appointment.name}</Text>
+          <Text style={styles.dateText}>{appointment.date}</Text>
+          <Text style={styles.timeText}>{appointment.time}</Text>
+          <Pressable
+            onPress={() =>
+              OnReschedule(
+                appointment._id,
+                appointment.name,
+                appointment.date,
+                appointment.time,
+              )
+            }
+            style={styles.editButton}>
+            <Text style={styles.editText}>Reschedule</Text>
+          </Pressable>
 
-                </Pressable>
-                
-                <Pressable style={styles.delete} onPress={()=>OnDelete(appointment._id)}>
-                    <Text style={styles.cancelTicket}>Cancel Booking</Text>
-                </Pressable>
-
-
+          <Pressable
+            style={styles.delete}
+            onPress={() => OnDelete(appointment._id)}>
+            <Text style={styles.cancelTicket}>Cancel Booking</Text>
+          </Pressable>
         </View>
-        ))}
-        </View>
-
-    )
-}
+      ))}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    header:{
-        fontSize:20,
-        fontWeight:'bold',
-        marginTop:20,
-        marginLeft:20,
-        marginBottom:20,
-        textAlign:'center'
-    },
-    appointment:{
-        margin:10,
-        padding:10,
-        borderRadius:10,
-        backgroundColor:'#fff',
-        elevation:5,
-        shadowColor:'#000',
-    },
-    delete:{
-        backgroundColor:'grey',
-        padding:10,
-        borderRadius:10,
-        elevation:5,
-        shadowColor:'#000',
-        shadowOffset:{width:0,height:2},
-        color:'#fff',
-        width:'45%',
-        alignSelf:'flex-end',
-        marginTop:10,
-        backgroundColor:'grey',
-    },
-    cancelTicket:{
-        color:'#fff',
-        fontSize:20,
-        fontWeight:'bold',
-
-    },
-    nameText:{
-        fontSize:20,
-        fontWeight:'bold',
-        marginBottom:10,
-    },
-    editButton:{
-        backgroundColor:'#fff',
-        padding:10,
-        borderRadius:10,
-        elevation:5,
-        shadowColor:'#000',
-        shadowOffset:{width:0,height:2},
-        color:'#000',
-        width:'45%',
-        alignSelf:'flex-start',
-        marginTop:10,
-        backgroundColor:'#fff',
-        top:50,
-    },
-})
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginLeft: 20,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  appointment: {
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    elevation: 5,
+    shadowColor: '#000',
+  },
+  delete: {
+    backgroundColor: 'grey',
+    padding: 10,
+    borderRadius: 10,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    color: '#fff',
+    width: '45%',
+    alignSelf: 'flex-end',
+    marginTop: 10,
+    backgroundColor: 'grey',
+  },
+  cancelTicket: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  nameText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  editButton: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    color: '#000',
+    width: '45%',
+    alignSelf: 'flex-start',
+    marginTop: 10,
+    backgroundColor: '#fff',
+    top: 50,
+  },
+});
 export default MyAppoints;
--- appointment/src/screens/RescheduleScreen.js
+++ appointment/src/screens/RescheduleScreen.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button, Alert, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Alert,
+  TextInput,
+  TouchableHighlight,
+  Pressable,
+} from 'react-native';
 import Api from '../api/Api';
+import TimeSlot from '../db/TimeSlot';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
 
 const RescheduleScreen = ({route, navigation}) => {
-  const [appointments, setAppointments] = React.useState([]);
-  const [id, setId] = React.useState([]);
+  const {id: id} = route.params;
+  const {name: appointmentName} = route.params;
+  const {date: appointmentDate} = route.params;
+  const {time: appointmentTime} = route.params;
 
-  const [name, setName] = React.useState('');
-  const {id: id1} = route.params;
+  const [appointments, setAppointments] = React.useState([]);
+  // const [id, setId] = React.useState([]);
+
+  const [name1, setName] = React.useState(`${appointmentName}`);
+  const [date, setDate] = React.useState('Select Date');
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+  const [show, setShow] = React.useState(false);
+  const [time1, setTime] = React.useState(`${appointmentTime}`);
+  const [mode, setMode] = React.useState('date');
+  const [timeSlot, setTimeSlot] = React.useState([]);
+  const [select, setSelect] = React.useState('');
+
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setTimeSlot(TimeSlot);
+  });
 
   const reschedule = () => {
-    Api.put(`/appoints/${id1}`, {name}).then(res => {
+    Api.put(`/appoints/${id}`, {
+      name: name1,
+      date: selectedDate.toDateString(),
+      time: time1,
+    }).then(res => {
       console.log(res);
       setName('');
       navigation.navigate('MyAppoints', appointments);
@@ -19,39 +53,101 @@ const RescheduleScreen = ({route, navigation}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Reschedule Appointment</Text>
-      <Text style={styles.label}>Name</Text>
+      <Text style={styles.label}>{name1}</Text>
       <TextInput
         style={styles.input}
-        value={name}
+        value={name1}
         onChangeText={text => setName(text)}
       />
+      <Text style={styles.label}>Date</Text>
+
+      <View style={styles.datePicker}>
+        <View style={styles.CalendarIconStyle}></View>
+        <TouchableHighlight
+          onPress={() => setShow(true)}
+          style={styles.datePickerButton}>
+          <Text style={styles.datePickerButtonText}>
+            <FontAwesome
+              name="calendar"
+              size={20}
+              color="#fff"
+              style={{marginRight: 10, marginLeft: 10}}
+            />
+            {date === 'Select Date'
+              ? '    Select Date'
+              : selectedDate.toDateString()}
+          </Text>
+        </TouchableHighlight>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={selectedDate}
+            mode={mode}
+            display="default"
+            onChange={(e, newDate) => {
+              setShow(false);
+              setSelectedDate(newDate);
+              setDate('New Date');
+            }}
+            minimumDate={new Date()}
+            maximumDate={new Date().setDate(new Date().getDate() + 1)}
+          />
+        )}
+      </View>
+
+      {date === 'Select Date' ? null : (
+        <View style={styles.timeSlot}>
+          {timeSlot.map(slot => {
+            return (
+              <>
+                <Pressable
+                  onPress={() => setSelect(slot)}
+                  style={[
+                    styles.timeSlotItem,
+                    select === slot ? styles.timeSlotItemSelect : null,
+                  ]}>
+                  <View>
+                    <FontAwesome name="clock-o" size={20} color="#009387" />
+                  </View>
+
+                  <Text style={styles.timeSlotItemText}>{slot.startTime}</Text>
+                </Pressable>
+              </>
+            );
+          })}
+        </View>
+      )}
+
       <Button title="Reschedule" onPress={reschedule} />
     </View>
   );
 };
 
-styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  text: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  input: {
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    padding: 10,
-    marginBottom: 20,
-  },
-  label: {
+  header: {
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  input: {
+    borderColor: '#009387',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    width: '100%',
+    marginBottom: 20,
+    backgroundColor: '#888',
   },
 });
 
