624c70b277681601da6371f6d0b66e7a00940a42
Raja <kingraja673@gmail.com>
2022-09-12 19:17:52 +0530

    adding sign in not complete yet

--- appointment/App.js
+++ appointment/App.js
@@ -18,20 +18,95 @@ import {
 } from 'react-native';
 import AppNavContainer from './src/navigation/index';
 import RNBootSplash from "react-native-bootsplash";
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AuthContext } from './src/components/context';
 const App = ()=>{
+   const [isLoading, setIsLoading] = React.useState(true);
+   const [userToken, setUserToken] = React.useState(null); 
   useEffect(() => {
     setTimeout(() => {
       RNBootSplash.hide({fade: true});
     }, 3000);
   }, []);
+  const initialLoginState = {
+    isLoading: true,
+    userName: null,
+    userToken: null,
+  };
+
+  const loginReducer = (prevState, action) => {
+    switch( action.type ) {
+      case 'RETRIEVE_TOKEN': 
+        return {
+          ...prevState,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'LOGIN': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'LOGOUT': 
+        return {
+          ...prevState,
+          userName: null,
+          userToken: null,
+          isLoading: false,
+        };
+      case 'REGISTER': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+    }
+  };
+
+  const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
+
+  const authContext = React.useMemo(() => ({
+    signIn: async(foundUser) => {
+      setUserToken('fgkj');
+      setIsLoading(false);
+      // const userToken = String(foundUser[0].userToken);
+      // const userName = foundUser[0].username;
+      
+      try {
+        await AsyncStorage.setItem('userToken', userToken);
+      } catch(e) {
+        console.log(e);
+      }
+      // console.log('user token: ', userToken);
+      dispatch({ type: 'LOGIN', id: userName, token: userToken });
+    },
+    signOut: async() => {
+      // setUserToken(null);
+      // setIsLoading(false);
+      try {
+        await AsyncStorage.removeItem('userToken');
+      } catch(e) {
+        console.log(e);
+      }
+      dispatch({ type: 'LOGOUT' });
+    },
+    signUp: () => {
+      // setUserToken('fgkj');
+      // setIsLoading(false);
+    },
+    
+  }), []);
 
 
   return(
     <>
-      
+        <AuthContext.Provider value={authContext}>
         <AppNavContainer>
         </AppNavContainer>
+        </AuthContext.Provider>
     </>
   
   )
